37b18dfa890162b95ef9f85758fa97bf50ea4f4e
Dipesh chauhan <dpshchhn@gmail.com>
2023-02-19 21:20:06 +0545

    responsive navbar

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
-console.log("script loadeds")
+const hamburgerButton = document.getElementById("hamburger");
+const closeButton = document.getElementById("closeButton");
+const mobileMenu = document.getElementById("mobileMenu");
+
+hamburgerButton.addEventListener("click" , () => {
+    mobileMenu.classList.remove("hide");
+})
+
+closeButton.addEventListener("click" , () => {
+    mobileMenu.classList.add("hide");
+})
